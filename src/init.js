7902a996e541bdd9bc7f995bf1bad5178450a38b
Parker Jones <pjones@altavian.com>
2018-02-13 16:45:29 -0500

    saved file, missing documentation

--- src/init.js
+++ src/init.js
@@ -3,6 +3,10 @@ const numCPUs = require('os').cpus().length;
 import master from './master';
 import worker from './worker';
 const Queue = require('bee-queue');
+
+// NOTE: start a named queue here, our name is webhooks.
+// bee-queue is a lightweight queue manager that uses redis to persist it's work queue,
+// this way it can distribute the work out to workers
 const queue = new Queue('webhooks', {
   redis: {
     host: process.env.REDIS_URL || 'services.fetch.altavian.local',
@@ -10,7 +14,7 @@ const queue = new Queue('webhooks', {
     db: 2
   }
 });
-
+// NOTE: throng is a light weight child process manager, reducers boilerplate code
 export default throng({
   workers: numCPUs > 4 ? numCPUs : 4,
   master: () => master(queue),
